5d8088ed294bf0a0a6aa1f09948388cd9d4b5609
Gulmammadov <ayaz.gulmammadov@accenture.com>
2019-07-30 12:28:16 +0300

    changed build script

--- Scripts/BuildSolution.ps1
+++ Scripts/BuildSolution.ps1
@@ -7,8 +7,8 @@ Param(
 Write-Host "Restore nuget..."
 & ("{0}{1}" -f $ToolsDir, "\NuGet\4.4.1\x64\nuget.exe") restore $Solution
 
-$ms = (Get-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7\).'15.0'
-$MSBuildPath = ("{0}{1}" -f $ms, 'MSBuild\15.0\Bin\MSBuild.exe')
+#$ms = (Get-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7\).'15.0'
+$MSBuildPath = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe'
 
 Write-Host "Start build solution..."
 & $MSBuildPath $Solution "/p:DeployOnBuild=true;Configuration=Release;DeployDefaultTarget=WebPublish;WebPublishMethod=FileSystem;SkipInvalidConfigurations=true;publishUrl=$destFolder"
